f25e7a80339223296e91f9a7f6b82df32367f723
demigod <divyanshjain.2206@gmail.com>
2023-11-03 05:34:49 +0530

    add login signup and integrate server

--- Client/src/pages/login.js
+++ Client/src/pages/login.js
@@ -1,25 +1,93 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../stylesheets/login.css";
-function Login() {
-  const [isSignUp, setIsSignUp] = useState(false);
-  const [isSignIn, setIsSignIn] = useState(false);
 
-  const handleSignUpClick = () => {
-    setIsSignUp(true);
-    setIsSignIn(false);
+function Login() {
+  let navigate = useNavigate();
+  const [responseText, setResponseText] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
 
-  const handleSignInClick = () => {
-    setIsSignIn(true);
-    setIsSignUp(false);
+  const handleSignUpClick = async (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(name);
+    console.log(password);
+
+    const data = {
+      email: email,
+      name: name,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:5000/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.tokens.access.token);
+        navigate("home");
+      } else {
+        setResponseText(`Error: ${response.status} - ${response.statusText}`);
+      }
+    } catch (error) {
+      setResponseText(`Error: ${error.message}`);
+    }
+  };
+
+  const handleSignInClick = async (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:5000/v1/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.tokens.access.token);
+        navigate("home");
+      } else {
+        setResponseText(`Error: ${response.status} - ${response.statusText}`);
+      }
+    } catch (error) {
+      setResponseText(`Error: ${error.message}`);
+    }
   };
 
   return (
     <>
-      <div id="info">
-        
-      </div>
-      <div id="container">
+      <div id="info"></div>
+      <div id="container" style={{ border: "1rem", borderColor: "white" }}>
         <div id="cover">
           <h1 className="sign-up">Hello, Friend!</h1>
           <p className="sign-up">
@@ -41,10 +109,20 @@ function Login() {
         <div id="login">
           <h1>Sign In</h1>
           <p>or use your email account:</p>
-          <form>
-            <input type="email" placeholder="Email" autocomplete="off" />
+          <form onSubmit={handleSignInClick}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleEmailChange}
+            />
             <br></br>
-            <input type="password" placeholder="Password" autocomplete="off" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handlePasswordChange}
+            />
             <br></br>
             <a id="forgot-pass" href="#">
               Forgot your password?
@@ -56,12 +134,28 @@ function Login() {
         <div id="register">
           <h1>Create Account</h1>
           <p>or use your email for registration:</p>
-          <form>
-            <input type="text" placeholder="Name" autocomplete="off" />
+          <form onSubmit={handleSignUpClick}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={handleNameChange}
+            />
             <br></br>
-            <input type="email" placeholder="Email" autocomplete="off" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleEmailChange}
+            />
             <br></br>
-            <input type="password" placeholder="Password" autocomplete="off" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handlePasswordChange}
+              autocomplete="off"
+            />
             <br></br>
             <input className="submit-btn" type="submit" value="Sign Up" />
           </form>
