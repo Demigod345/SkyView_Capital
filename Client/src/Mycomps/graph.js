3253d941cb44483c288d9b8b8a3f9e8851b98d2a
demigod <divyanshjain.2206@gmail.com>
2023-11-04 22:15:09 +0530

    add graph and api call

--- Client/src/Mycomps/graph.js
+++ Client/src/Mycomps/graph.js
@@ -1,33 +1,90 @@
-import React from "react";
-import {
-  XYPlot,
-  XAxis,
-  YAxis,
-  LineSeries,
-  VerticalGridLines,
-  HorizontalGridLines,
-} from "react-vis";
-import "../../node_modules/react-vis/dist/style.css";
+import React, { useEffect, useState } from "react";
 import "../stylesheets/graph.css";
+import axios from "axios";
+import CanvasJSReact from "@canvasjs/react-stockcharts";
+var CanvasJSStockChart = CanvasJSReact.CanvasJSStockChart;
+
 export default function Graph() {
-  const data = [
-    { x: 0, y: 8 },
-    { x: 1, y: 5 },
-    { x: 2, y: 4 },
-    { x: 3, y: 9 },
-    { x: 4, y: 1 },
-  ];
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const apiKey = `${process.env.REACT_APP_FINANCE_API_KEY}`;
+    const symbol = "AAPL";
+
+    fetch(
+      `https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=all&token=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("data aa gya");
+
+    //DO CHANGES WITH DATA HERE
+  }, [data]);
+
+  //data aa gya
+
+  const options = {
+    title: {
+      text: "CanvasJS React StockChart",
+    },
+    charts: [
+      {
+        data: [
+          {
+            type: "line",
+            dataPoints: [
+              { x: new Date("2018-01-01"), y: 71 },
+              { x: new Date("2018-02-01"), y: 55 },
+              { x: new Date("2018-03-01"), y: 50 },
+              { x: new Date("2018-04-01"), y: 65 },
+              { x: new Date("2018-05-01"), y: 95 },
+              { x: new Date("2018-06-01"), y: 68 },
+              { x: new Date("2018-07-01"), y: 28 },
+              { x: new Date("2018-08-01"), y: 34 },
+              { x: new Date("2018-09-01"), y: 14 },
+              { x: new Date("2018-10-01"), y: 71 },
+              { x: new Date("2018-11-01"), y: 55 },
+              { x: new Date("2018-12-01"), y: 50 },
+              { x: new Date("2019-01-01"), y: 34 },
+              { x: new Date("2019-02-01"), y: 50 },
+              { x: new Date("2019-03-01"), y: 50 },
+              { x: new Date("2019-04-01"), y: 95 },
+              { x: new Date("2019-05-01"), y: 68 },
+              { x: new Date("2019-06-01"), y: 28 },
+              { x: new Date("2019-07-01"), y: 34 },
+              { x: new Date("2019-08-01"), y: 65 },
+              { x: new Date("2019-09-01"), y: 55 },
+              { x: new Date("2019-10-01"), y: 71 },
+              { x: new Date("2019-11-01"), y: 55 },
+              { x: new Date("2019-12-01"), y: 50 },
+            ],
+          },
+        ],
+      },
+    ],
+    navigator: {
+      slider: {
+        minimum: new Date("2018-07-01"),
+        maximum: new Date("2023-11-03"),
+      },
+    },
+  };
 
   return (
     <div className="graph">
       <div style={{ width: "500px" }}>
-        <XYPlot height={400} width={400}>
-          <LineSeries data={data} />
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <XAxis title="time " />
-          <YAxis title="stock price" />
-        </XYPlot>
+        <div>
+          <CanvasJSStockChart options={options} />
+        </div>
       </div>
       <div style={{ width: "600px" }}>
         <table align="center">
