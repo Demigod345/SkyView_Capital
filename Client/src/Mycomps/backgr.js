06b80a68677fd609bf52c172149bc9fb1776361a
Aaditya Aren <aadityaaren01@Aadityas-MacBook-Air.local>
2023-11-07 06:47:47 +0530

    css changes

--- Client/src/Mycomps/backgr.js
+++ Client/src/Mycomps/backgr.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import im from './images/image.svg';
+import React from "react";
+import im from "./images/image.svg";
 import im1 from "./images/bgtl.svg";
 import im2 from "./images/bgtr.svg";
 import im3 from "./images/bgbl.svg";
@@ -9,17 +9,16 @@ export default function HomePage() {
   return (
     <div className="bgmain">
       <div className="image-row">
-        <img src={im} className="full-width-image" alt="" />
+        <img src={im} className="full-width-image"  />
       </div>
-      <div className="image-row">
-        <img src={im1} className="img1" alt="" />
-        <img src={im2} className="img2" alt="" />
-      </div>
-      <div className="image-row"></div>
-        <img src={im3} className="img3" alt="" />
-        <img src={im4} className="img4" alt="" />
-      <div/>
-      </div>
-    
+      <div className="image-row-icons">
+      <img src={im4} className="img1" />
+    <img src={im2} className="img2" />
+  </div>
+  <div className="image-row-icons">
+    <img src={im3} className="img3" />
+    <img src={im1} className="img4" />
+  </div>
+    </div>
   );
 }
