567e2dc60aaaadff15d2c55e35a22a4f071d8a67
Ishaan Jain <ishaan_j@cs.iitr.ac.in>
2023-11-07 23:04:11 +0530

    update background

--- Client/src/Mycomps/backgr.js
+++ Client/src/Mycomps/backgr.js
@@ -8,17 +8,26 @@ import im4 from "./images/bgbr.svg";
 export default function HomePage() {
   return (
     <div className="bgmain">
-      <div className="image-row">
+      <div >
         <img src={im} className="full-width-image"  />
       </div>
-      <div className="image-row-icons">
-      <img src={im4} className="img1" />
-    <img src={im2} className="img2" />
-  {/* </div> */}
-  {/* <div className="image-row-icons"> */}
-    <img src={im3} className="img3" />
-    <img src={im1} className="img4" />
-  </div>
+      {/* <div class="bg-image-container">
+        <img src={im} alt="Background" />
+      </div> */}
+
+       {/* <div className="image-row-icons">
+        <img src={im4} className="img1" />
+        <img src={im2} className="img2" />
+        <img src={im3} className="img3" />
+        <img src={im1} className="img4" />
+      </div>  */}
+
+      <div className="image-row-container">
+        <img src={im4} className="img5" />
+        <img src={im2} className="img5" />
+        <img src={im3} className="img5" />
+        <img src={im1} className="img5" />
+      </div>
     </div>
   );
 }
